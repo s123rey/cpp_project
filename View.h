ab47e310b4c1abf320d801ed5c6d8a99389e490b
s123rey <94102982+s123rey@users.noreply.github.com>
2021-11-11 16:36:32 +0530

    Add files via upload

--- View.h
+++ View.h
@@ -1,19 +1,16 @@
-#include<iostream>
-//#include "Controller_base.h"
 #ifndef VIEW_H
 #define VIEW_H
 
-#include "Controller_base.h"
-#include "Model.h"
-class Controller_base;
-class Model;
+#include "../Controller/Controller_base.h"
+#include "../Model/Model.h"
+
 class View 
 {
 	public:
-	virtual ~View();
-	virtual void register_view(Controller_base* ptr_c) = 0;
-	virtual void update() = 0;
-	virtual void get_model(Model *ptr_model) = 0;
-	
+		virtual ~View() { }
+		virtual void register_view(Controller_base* ptr_c) = 0;
+		virtual void update() = 0;
+		virtual void get_model(Model *ptr_model) = 0;
 };
+
 #endif
